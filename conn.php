38d10dcca4703cf70868b24ce49b6e8b59e5d932
luis3518 <48697966+Luis3518@users.noreply.github.com>
2019-11-05 14:35:44 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 // Variables de conexion
-$dbhost	= "localhost";	   // localhost or IP
-$dbuser	= "root";		  // database username
-$dbpass	= "";		     // database password
+$dbhost	= "sql303.260mb.net";	   // localhost or IP
+$dbuser	= "n260m_24748450";		  // database username
+$dbpass	= "Ne1557343133";		     // database password
 $dbname	= "phplogin";    // database name
 ?>
